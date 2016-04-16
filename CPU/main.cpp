ceb10b3889dc0cf380987e641172962ddd7f8ea0
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-04-16 15:51:19 -0700

    init main

---
+++ CPU/main.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Danyang Zhang on 16/04/2016.
+//
+
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "hello" << endl;
+    cout << "hello" << endl;
+    int abcd = 0;
+    for (int i = 0; i < 100; i++) {
+        cout << "hello" << endl;
+        cout << "hello" << endl;
+        abcd = 0;
+    }
+
+}
