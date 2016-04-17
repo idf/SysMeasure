bda14f88f4c23df9a7e1c44a4a5c4a41aaa6c8bc
Mubin Wang <mubin.w@gmail.com>
2016-04-16 19:55:50 -0700

    dummy for pull

--- CPU/CpuMeasurer.cpp
+++ CPU/CpuMeasurer.cpp
@@ -5,7 +5,23 @@
 #include "CpuMeasurer.h"
 
 void CpuMeasurer::measurementOverhead() {
+    unsigned long clock_total = 0;
+    unsigned long long start, end;
+    unsigned long long diff;
+//    vector<double> ret;
 
+    for (auto i = 0; i < TIMES_PER_EXPERIMENT; i++) {
+        start = rdtscStart();
+        end = rdtscEnd();
+        diff = end - start;
+//        ret.push_back(diff);
+
+        clock_total = clock_total + diff;
+
+        cout << diff << endl;
+    }
+
+    cout << (clock_total / (float)TIMES_PER_EXPERIMENT) << endl;
 }
 
 void CpuMeasurer::procedureCallOverhead() {
