83c6e237b34abd12ab396bab2fb64fdb3c18c4b5
Mubin Wang <mubin.w@gmail.com>
2016-04-16 19:49:10 -0700

    refactored CpuMeasurer

--- CPU/CpuMeasurer.cpp
+++ CPU/CpuMeasurer.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "CpuMeasurer.h"
+
+void CpuMeasurer::measurementOverhead() {
+
+}
+
+void CpuMeasurer::procedureCallOverhead() {
+
+}
+
+void CpuMeasurer::systemCallOverhead() {
+
+}
+
+void CpuMeasurer::taskCreationTime() {
+
+}
+
+void CpuMeasurer::contextSwitchTime() {
+
+}
+
+
+
+
+
+
+
+
+
--- CPU/main.cpp
+++ CPU/main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 int main() {
-    freopen("data/output.txt","w", stdout);
+    freopen("output.txt","w", stdout);
 
     cout << "Measurement Overhead" << endl;
 
