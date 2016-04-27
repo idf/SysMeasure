f9f337a8065f2e7e1b82e5a9092d3e2c454d6ed1
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-04-27 13:09:59 -0700

    mem read latency rand

--- Mem/mem.cpp
+++ Mem/mem.cpp
@@ -9,54 +9,60 @@
 
 using namespace std;
 
-void cache_demo() {
+int* cache_demo() {
     // https://gcc.gnu.org/onlinedocs/gcc-3.4.4/gcc/Optimize-Options.html
     int sz = 40960;
+    int* A;
+    A = (int *) malloc(sz * sizeof(int));
+
+    int* p = A;
     cout << "size of int: " << sizeof(int) << endl;
     for(auto k = 1; k <= 2048; k *= 2) {
-        int *A;
-        A = (int *) malloc(sz * sizeof(int));
-
-        A[0] = 0;
+        p[0] = 0;
         uint64_t start = rdtscStart();
-        A[k] *= 2;
-        A[2*k] *= 2;
-        A[3*k] *= 2;
-        A[4*k] *= 2;
-        A[5*k] *= 2;
-        A[6*k] *= 2;
-        A[7*k] *= 2;
-        A[8*k] *= 2;
+        p[k] *= 2;
+        p[2*k] *= 2;
+        p[3*k] *= 2;
+        p[4*k] *= 2;
+        p[5*k] *= 2;
+        p[6*k] *= 2;
+        p[7*k] *= 2;
+        p[8*k] *= 2;
         uint64_t end = rdtscEnd();
 
         cout << k << "\t";
         cout << float (end - start) << endl;
 
-        free(A);
+        p = A;
     }
+    return p;
 }
 
 
-int ** latency(long sz) {
+int ** latency(long sz, long itr_cnt) {
     cout << "Test Memory Read Latency" << endl;
     int** A = new int* [sz];
     int ** ret = NULL ;
     for(auto k = 1; k <= 2048; k *= 2) {
-        int ** p = A;
         for(auto i = 0; i< sz; i++) {
             int index = (i/k+1)*k+rand()%k;
             index %= sz;
             A[i] = (int *) &A[index];
         }
 
+        int ** p = A;
+        int one_pass_cnt = sz/k + 1;
         uint64_t start = rdtscStart();
-        for(auto i=0; i < sz; i++) {
-            p = (int **) *p;
+        for(auto i=0; i < itr_cnt; i++) {
+            for(auto j=0; j < one_pass_cnt; j++) {
+                p = (int **) *p;
+            }
+            p = A;
         }
         uint64_t end = rdtscEnd();
         ret = p;
         cout << k << "\t";
-        cout << float (end - start)/sz << endl;
+        cout << float (end - start) / (itr_cnt * one_pass_cnt) << endl;
     }
     return ret;
 }
@@ -65,8 +71,9 @@ int main() {
     const bool DEBUG = true;
 
     if (!DEBUG) {
-        freopen("CPU/result/output.txt","w", stdout);
+        freopen("Mem/result/output.txt","w", stdout);
     }
-    cache_demo();
-    latency();
+
+    // cout << cache_demo();
+    cout << latency(409600, 1000);
 }
