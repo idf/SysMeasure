2d54d49cbf1e693740311b84406fcbf2fb08f1f4
Mubin Wang <mubin.w@gmail.com>
2016-04-27 21:01:37 -0700

    added bandwidth experiement

---
+++ Mem/bandwidth.cpp
@@ -0,0 +1,156 @@
+//
+// Created by MuyBien on 4/27/16.
+//
+
+#include <iostream>
+#include <assert.h>
+#include <chrono>
+#include <immintrin.h>
+#include "../common.h"
+
+#ifdef __AVX__
+#include <immintrin.h>
+#endif
+
+using namespace std;
+
+#define MB (1024*1024LL)
+#define GB (1024*1024*1024LL)
+#define SIZE (1*GB)
+#define EXPERIMENT 1
+#define REPETITION 10
+
+typedef __int128_t TYPE;
+//typedef long long TYPE;
+//typedef int TYPE;
+//typedef char TYPE;
+
+
+TYPE data[SIZE/sizeof(TYPE)] __attribute__((aligned (32)));
+
+double calculate_bandwidth(double size, double seconds){
+    return size / seconds;
+}
+
+void read_memory_loop(){
+    TYPE val = 0;
+    int bound = SIZE / sizeof(TYPE);
+    for(int i=0; i<bound; i++){
+        val = data[i];
+    }
+    // make use of val to prevent compiler optimization
+    assert(!(val==0));
+}
+
+void read_memory_loop_experiment(){
+    cout << "====== read memory loop experiment ======" <<endl;
+//    for(int i=0; i<EXPERIMENT; i++){
+//        cout << "Experiment #" << i << ":" << endl;
+
+        double min_duration = 0;
+        for(int j=0; j<REPETITION; j++){
+            auto start = chrono::steady_clock::now();
+            read_memory_loop();
+            auto end = chrono::steady_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end-start;
+//            cout << elapsed_seconds.count() << endl;
+            if(j==0) min_duration = elapsed_seconds.count();
+            else min_duration = min(min_duration, elapsed_seconds.count());
+        }
+        cout << calculate_bandwidth((double) SIZE / GB, min_duration) << endl;
+//    }
+}
+
+__attribute__ ((target ("sse4.2")))
+__attribute__ ((target ("xsave")))
+__m256 read_memory_avx() {
+    __m256* varray = (__m256*) data;
+
+    __m256 accum = _mm256_set1_ps((float)  0x00000000);
+    for (int i = 0; i < SIZE/sizeof(__m256i); i++) {
+        accum = _mm256_add_ps(varray[i], accum);
+    }
+
+    assert(!_mm256_testz_ps(accum, accum));
+    return accum;
+}
+
+void read_memory_avx_experiment(){
+    cout << "====== read memory avx experiment ======" <<endl;
+//    for(int i=0; i<EXPERIMENT; i++){
+//        cout << "Experiment #" << i << ":" << endl;
+
+        double min_duration = 0;
+        for(int j=0; j<REPETITION; j++){
+            auto start = chrono::steady_clock::now();
+            read_memory_avx();
+            auto end = chrono::steady_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end-start;
+            if(j==0) min_duration = elapsed_seconds.count();
+            else min_duration = min(min_duration, elapsed_seconds.count());
+        }
+        cout << calculate_bandwidth((double) SIZE / GB, min_duration) <<endl;
+//    }
+}
+
+void write_memory_loop(){
+    TYPE val = 0;
+    int bound = SIZE/sizeof(TYPE);
+    for(int i=0; i<bound; i++){
+        data[i] = 0;
+    }
+    // make use of val to prevent compiler optimization
+    assert(val==0);
+}
+
+void write_memory_loop_experiment(){
+    cout << "====== write memory loop experiment ======" <<endl;
+
+//    for(int i=0; i<EXPERIMENT; i++){
+//        cout << "Experiment #" << i << ":" << endl;
+        double min_duration = 0;
+        for(int j=0; j<REPETITION; j++){
+            auto start = chrono::steady_clock::now();
+            write_memory_loop();
+            auto end = chrono::steady_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end-start;
+            if(j==0) min_duration = elapsed_seconds.count();
+            else min_duration = min(min_duration, elapsed_seconds.count());
+        }
+        cout << calculate_bandwidth((double) SIZE / GB, min_duration) <<endl;
+//    }
+}
+
+void write_memory_memset_experiment(){
+    cout << "====== write memory memset experiment ======" <<endl;
+//    for(int i=0; i<EXPERIMENT; i++){
+//        cout << "Experiment #" << i << ":" << endl;
+
+        double min_duration = 0;
+        for(int j=0; j<REPETITION; j++){
+            auto start = chrono::steady_clock::now();
+            memset(data, 0, SIZE);
+            auto end = chrono::steady_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end-start;
+            if(j==0) min_duration = elapsed_seconds.count();
+            else min_duration = min(min_duration, elapsed_seconds.count());
+        }
+        cout << calculate_bandwidth((double) SIZE / GB, min_duration) <<endl;
+//    }
+}
+
+
+int main(){
+    memset(data, 0xFF, SIZE);
+
+    cout << "data array size is: " << sizeof(data) << " bytes " << endl;
+//    cout << sizeof(__int128_t) <<endl;
+//    cout << sizeof(long long ) <<endl;
+//    cout << sizeof(uint64_t) << endl;
+
+    read_memory_loop_experiment();
+    read_memory_avx_experiment();
+    write_memory_loop_experiment();
+    write_memory_memset_experiment();
+}
+
