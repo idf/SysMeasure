0cdd5877c6fb9b17c35b7eb6c0ce763aebc6180d
Mubin Wang <mubin.w@gmail.com>
2016-05-15 17:23:22 -0700

    dummy

--- Networking/setup.py
+++ Networking/setup.py
@@ -4,9 +4,9 @@ import time
 
 if __name__ == "__main__":
     HOST = sys.argv[1]      # The host
-    PORT = sys.argv[2]      # The same port as used by the server
+    PORT = int(sys.argv[2])      # The same port as used by the server
 
-    for i in xrange(0100):
+    for i in xrange(100):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         start = time.time()
         s.connect((HOST, PORT))
--- Networking/teardown.py
+++ Networking/teardown.py
@@ -4,13 +4,13 @@ import time
 
 if __name__ == "__main__":
     HOST = sys.argv[1]      # The host
-    PORT = sys.argv[2]      # The same port as used by the server
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    PORT = int(sys.argv[2])      # The same port as used by the server
 
     for i in xrange(100):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((HOST, PORT))
         start = time.time()
         s.close()
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         end = time.time()
         print (end - start) * 1000
