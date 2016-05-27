cadcd63c4010f36120d7d2b2cb730aec9d8e2827
Mubin Wang <mubin.w@gmail.com>
2016-05-27 15:51:24 -0700

    dummy

--- FS/createfile.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-
-with open(sys.argv[1], 'wb') as fout:
-    fout.write(os.urandom(int(sys.argv[2]) * 1024 * 1024))
--- FS/filecache.cpp
+++ FS/filecache.cpp
@@ -3,4 +3,63 @@
 //
 
 
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+const off_t BLOCKSIZE = 4*1024;
+
+int main(int argc, const char * argv[])
+{
+    void* buf = malloc(BLOCKSIZE);
+    const off_t FILESIZE = atoll(argv[1]);
+    int fd = open(argv[2], O_RDONLY | O_SYNC);
+
+    //We first seek to the last block in the file
+    lseek(fd, FILESIZE - 1, SEEK_SET);
+    off_t totalBytes = 0;
+
+    //Then we read the file from end to start to put the file into cache
+    while(1){
+        lseek(fd, -2*BLOCKSIZE, SEEK_CUR);
+        ssize_t bytes = read(fd, buf, BLOCKSIZE);
+        if (bytes <= 0 || totalBytes >= FILESIZE)
+            break;
+        totalBytes += bytes;
+    }
+
+    close(fd);
+
+    //We close the file and read it again
+    fd = open(argv[2], O_RDONLY| O_SYNC);
+    totalBytes = 0;
+
+    //Still seek the last block in the file
+    lseek(fd, FILESIZE - 1, SEEK_SET);
+
+    uint64_t st;
+    uint64_t ed;
+    uint64_t total_time = 0;
+
+    // Read backwards to avoid prefetching the blocks
+    while(1){
+        lseek(fd, -2*BLOCKSIZE, SEEK_CUR);
+//        st = rdtsc();
+        ssize_t bytes = read(fd, buf, BLOCKSIZE);
+//        ed = rdtsc();
+
+        total_time += ed - st;
+        if (bytes <= 0 || totalBytes >= FILESIZE)
+            break;
+        totalBytes += bytes;
+    }
+
+    close(fd);
+    free(buf);
+    double ans = (double)(total_time / (FILESIZE / BLOCKSIZE)) - 102;
+    printf("%lf\n",ans);
+    return 0;
+}
 
