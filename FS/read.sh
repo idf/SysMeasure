7b7850ffbd377f52d5eb83654751fd060722564c
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-06-02 20:36:49 -0700

    generalize read for both local and remote

--- FS/read.sh
+++ FS/read.sh
@@ -2,10 +2,22 @@
 
 function read_local_MB() {
    # param1: file size in MB
-   F=$1 && let SZ=1024*$F && sudo ./read_local $SZ ${F}MB.txt > result/local/${F}MB.txt
+   F=$1 && let SZ=1024*$F && sudo ./read $SZ ${F}MB.txt > result/local/${F}MB.txt
 }
 
 function read_local_KB() {
    # param1: file size in MB
-   SZ=$1 && sudo ./read_local $SZ ${SZ}KB.txt > result/local/${SZ}KB.txt
+   SZ=$1 && sudo ./read $SZ ${SZ}KB.txt > result/local/${SZ}KB.txt
+}
+
+remote_path="/Volumes/Someone's Public Folder/"
+
+function read_remote_MB() {
+   # param1: file size in MB
+   F=$1 && let SZ=1024*$F && sudo ./read $SZ ${remote_path}${F}MB.txt > result/local/${F}MB.txt
+}
+
+function read_remote_KB() {
+   # param1: file size in MB
+   SZ=$1 && sudo ./read $SZ ${remote_path}${SZ}KB.txt > result/local/${SZ}KB.txt
 }
