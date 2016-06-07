28ad60985f2c135cb4c0a9fc3843300dab784900
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-06-06 19:51:05 -0700

    remote read bash

--- FS/read.sh
+++ FS/read.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+sudo -v
 
 function read_local_MB() {
    # param1: file size in MB
@@ -10,14 +11,14 @@ function read_local_KB() {
    SZ=$1 && sudo ./read $SZ ${SZ}KB.txt > result/local/${SZ}KB.txt
 }
 
-remote_path="/Volumes/Someone's Public Folder/"
+remote_path="/Volumes/3.6-1/"
 
 function read_remote_MB() {
    # param1: file size in MB
-   F=$1 && let SZ=1024*$F && sudo ./read $SZ ${remote_path}${F}MB.txt > result/local/${F}MB.txt
+   F=$1 && let SZ=1024*$F && sudo ./read $SZ ${remote_path}${F}MB.txt > result/remote/${F}MB.txt
 }
 
 function read_remote_KB() {
    # param1: file size in MB
-   SZ=$1 && sudo ./read $SZ ${remote_path}${SZ}KB.txt > result/local/${SZ}KB.txt
+   SZ=$1 && sudo ./read $SZ ${remote_path}${SZ}KB.txt > result/remote/${SZ}KB.txt
 }
